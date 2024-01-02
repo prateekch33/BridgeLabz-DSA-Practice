a60e6766a616e5d3651e0bf74bf3cb5794d9cfc1
prateekch33 <prateekchandra27@outlook.com>
2024-01-02 16:44:43 +0530

    Anagrams

---
+++ src/main/java/org/example/AnagramDetection.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramDetection {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine().toLowerCase();
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine().toLowerCase();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The two strings are anagrams.");
+        } else {
+            System.out.println("The two strings are not anagrams.");
+        }
+    }
+
+    private static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert strings to character arrays
+        char[] charArray1 = str1.replaceAll("\\s", "").toCharArray();
+        char[] charArray2 = str2.replaceAll("\\s", "").toCharArray();
+
+        if (charArray1.length != charArray2.length) {
+            return false;
+        }
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
